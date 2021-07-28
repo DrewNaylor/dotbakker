db75e40eb9f738b9d1f077b125b4835a1bb0ed96
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-07-28 06:55:51 -0400

    Rename main form.

--- dotbakker-gui/My Project/Application.Designer.vb
+++ dotbakker-gui/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.dotbakker_gui.Form1
+            Me.MainForm = Global.dotbakker_gui.aaformMainWindow
         End Sub
     End Class
 End Namespace
