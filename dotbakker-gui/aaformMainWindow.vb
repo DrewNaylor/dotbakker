8adb6a2434c0c0c3ec87f4eb3b25a5a054f13901
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-07-28 07:15:16 -0400

    Getting the list of files.

--- dotbakker-gui/aaformMainWindow.vb
+++ dotbakker-gui/aaformMainWindow.vb
@@ -2,12 +2,18 @@
     Private Sub buttonAddDotBak_Click(sender As Object, e As EventArgs) Handles buttonAddDotBak.Click
         ' Copy the file in the path textbox and add ".bak#" to it.
         If IO.File.Exists(textboxFilePath.Text) Then
-            ' Assign a variable to store the directory.
-            Dim DirectoryToLookIn As String = IO.Path.GetDirectoryName(textboxFilePath.Text)
-
-            ' Loop through the files in the directory and find the lowest number.
             ' Some code based on this SO answer:
             ' https://stackoverflow.com/a/2216118
+            ' Other stuff from MSDN:
+            ' https://docs.microsoft.com/en-us/dotnet/api/system.io.path.getdirectoryname?view=netframework-4.8
+
+            ' Assign a variable to store the directory.
+            Dim DirectoryToLookIn As New IO.DirectoryInfo(IO.Path.GetDirectoryName(textboxFilePath.Text))
+
+            ' Store a list of all the files in the dir.
+            Dim FilesList As IO.FileInfo() = DirectoryToLookIn.GetFiles()
+
+            ' Loop through the files in the directory and find the lowest number.
             For Each FileInDir As 
         End If
     End Sub
