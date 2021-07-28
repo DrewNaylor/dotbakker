177d4864b525a451a9b98a26bee14c0eb2211955
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-07-28 07:06:23 -0400

    Start getting the file duplicated and use Option Strict.

--- dotbakker-gui/aaformMainWindow.vb
+++ dotbakker-gui/aaformMainWindow.vb
@@ -1,3 +1,8 @@
 ﻿Public Class aaformMainWindow
+    Private Sub buttonAddDotBak_Click(sender As Object, e As EventArgs) Handles buttonAddDotBak.Click
+        ' Copy the file in the path textbox and add ".bak#" to it.
+        If IO.File.Exists(textboxFilePath.Text) Then
 
+        End If
+    End Sub
 End Class
