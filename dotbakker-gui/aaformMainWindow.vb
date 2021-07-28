ef9ddcc954301c069ef6a654bec42ef00ee5cd6f
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-07-28 07:10:03 -0400

    Get directory name.

--- dotbakker-gui/aaformMainWindow.vb
+++ dotbakker-gui/aaformMainWindow.vb
@@ -2,6 +2,9 @@
     Private Sub buttonAddDotBak_Click(sender As Object, e As EventArgs) Handles buttonAddDotBak.Click
         ' Copy the file in the path textbox and add ".bak#" to it.
         If IO.File.Exists(textboxFilePath.Text) Then
+            ' Loop through the files in the directory and find the lowest number.
+            Dim DirectoryToLookIn As String = IO.Path.GetDirectoryName(textboxFilePath.Text)
+
 
         End If
     End Sub
