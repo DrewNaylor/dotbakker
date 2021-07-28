6121ea73c98d5b45cdf4ade168f84653f9e145a9
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-07-28 07:26:08 -0400

    Remove the quotes.

--- dotbakker-gui/aaformMainWindow.vb
+++ dotbakker-gui/aaformMainWindow.vb
@@ -1,14 +1,19 @@
 ﻿Public Class aaformMainWindow
     Private Sub buttonAddDotBak_Click(sender As Object, e As EventArgs) Handles buttonAddDotBak.Click
         ' Copy the file in the path textbox and add ".bak#" to it.
-        If IO.File.Exists(textboxFilePath.Text) Then
+
+        ' Trim the quotes from the file path.
+        Dim NoQuotes As String = textboxFilePath.Text.Trim(CChar(""""))
+
+        ' Check if the file exists.
+        If IO.File.Exists(NoQuotes) Then
             ' Some code based on this SO answer:
             ' https://stackoverflow.com/a/2216118
             ' Other stuff from MSDN:
             ' https://docs.microsoft.com/en-us/dotnet/api/system.io.path.getdirectoryname?view=netframework-4.8
 
             ' Assign a variable to store the directory.
-            Dim DirectoryToLookIn As New IO.DirectoryInfo(IO.Path.GetDirectoryName(textboxFilePath.Text))
+            Dim DirectoryToLookIn As New IO.DirectoryInfo(IO.Path.GetDirectoryName(NoQuotes))
 
             ' Store a list of all the files in the dir.
             Dim FilesList As IO.FileInfo() = DirectoryToLookIn.GetFiles()
